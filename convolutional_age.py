e3fc9dcba102143f5e601f324a699f6e399d958c
nigel <nigel.legg@gmail.com>
2015-02-11 15:48:39 +0000

    convo pred

--- convolutional_age.py
+++ convolutional_age.py
@@ -181,7 +181,7 @@ def evaluate_lenet5(datasets, imgh, imgw, nclass, L1_reg=0.00, L2_reg=0.0001,
     rng = numpy.random.RandomState(23455)
 
     train_set_x, train_set_y = datasets[0]
-    test_set_x, test_set_y = datasets[1]
+    test_set_x = datasets[1]
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]
