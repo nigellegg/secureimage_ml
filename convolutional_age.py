a8f28f7280a0d08992e57fc0d9acf644a0c321f5
nigel <nigel.legg@gmail.com>
2015-02-13 13:40:04 +0000

    test data

--- convolutional_age.py
+++ convolutional_age.py
@@ -123,11 +123,6 @@ def create_shared_dataset(dataset):
     def shared_dataset(data_xy, borrow=True):
         """ Function that loads the dataset into shared variables
 
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
         """
         data_x, data_y = data_xy
         shared_x = theano.shared(numpy.asarray(data_x,
@@ -152,7 +147,7 @@ def create_shared_dataset(dataset):
         shared_y = theano.shared(numpy.asarray(data_y,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
-        return shared_x, shared_y
+        return shared_x, T.cast(shared_y, 'int32')
 
     train_set, test_set = dataset
     test_set_x = shared_testset(test_set)
