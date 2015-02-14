3aa2b349c43a6bf1d9572fd75b8c9358a0fc2472
nigel <nigel.legg@gmail.com>
2015-02-14 13:29:19 +0000

    only train when no model

--- convolutional_age.py
+++ convolutional_age.py
@@ -460,7 +460,7 @@ if __name__ == '__main__':
 
     if os.path.isfile(model_file):
         input = open(model_file, 'rb')
-        params = pickle.load(input)
+        params = cPickle.load(input)
     else:
         params, test_error = evaluate_lenet5(shared_dataset, 40, 60, 5)
 
